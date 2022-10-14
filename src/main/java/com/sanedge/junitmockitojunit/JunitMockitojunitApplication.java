4f12a404f6ac3a8c15ffc387b93879dad2aa594e
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-10-14 21:32:59 +0700

    Adding

---
+++ src/main/java/com/sanedge/junitmockitojunit/JunitMockitojunitApplication.java
@@ -0,0 +1,13 @@
+package com.sanedge.junitmockitojunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitojunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMockitojunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanedge/junitmockitojunit/JunitMockitojunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanedge.junitmockitojunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMockitojunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
